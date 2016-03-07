0509144557e61e00c37e4a54496c0934688ccaa2
Eric Nelson <enelson@ancestry.com>
2016-03-07 13:43:21 -0700

    adding writer code and tests

--- src/main/java/com/enelson/monads/Writer.java
+++ src/main/java/com/enelson/monads/Writer.java
@@ -1,11 +1,54 @@
 package com.enelson.monads;
 
+import javaslang.Tuple2;
+import javaslang.collection.List;
+
+import java.util.function.Function;
+
 /**
  * Writer monad allows a log to be passed through chained calls.
  *
  * @author Eric Nelson
  * @since 1.0
  */
-public class Writer {
+public class Writer<A> {
 
+    private List<String> logs;
+    private A value;
+
+    private Writer(List<String> logs, A value) {
+        this.logs = logs;
+        this.value = value;
+    }
+
+    public static <A> Writer<A> of(String log, A value) {
+        return new Writer<>(List.of(log), value);
+    }
+
+    public <B> Writer<B> map(Function<A, B> f) {
+        return new Writer<>(logs, f.apply(value));
+    }
+
+    public <B> Writer<B> flatMap(Function<A, Writer<B>> f) {
+        Writer<B> mappedWriter = f.apply(value);
+        return new Writer<>(logs.appendAll(mappedWriter.logs), mappedWriter.value);
+    }
+
+    public Tuple2<List<String>, A> getContext() {
+        return new Tuple2<>(logs, value);
+    }
+
+    public List<String> getLogs() {
+        return logs;
+    }
+
+    public A getValue() {
+        return value;
+    }
+
+    @Override
+    public String toString() {
+        String logsString = logs.mkString(", ");
+        return "Writer("+logsString+", "+value+")";
+    }
 }
--- src/test/java/com/enelson/monads/WriterUnitTest.java
+++ src/test/java/com/enelson/monads/WriterUnitTest.java
@@ -1,4 +1,76 @@
 package com.enelson.monads;
 
+import javaslang.Tuple2;
+import javaslang.collection.List;
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
 public class WriterUnitTest {
+
+    @Test
+    public void testStaticOf() {
+        Writer<Integer> writer = Writer.of("Two", 2);
+
+        assertThat(writer).isNotNull();
+        assertThat(writer.getLogs()).isEqualTo(List.of("Two"));
+        assertThat(writer.getValue()).isEqualTo(2);
+    }
+
+    @Test
+    public void testMap() {
+        Writer<Integer> writer1 = Writer.of("Two", 2);
+        Writer<String> writer2  = writer1.map(String::valueOf);
+
+        assertThat(writer2).isNotNull();
+        assertThat(writer2.getLogs()).isEqualTo(List.of("Two"));
+        assertThat(writer2.getValue()).isEqualTo("2");
+    }
+
+    @Test
+    public void testFlatMap() {
+        Writer<Integer> writer = half(8).flatMap(i -> half(i));
+
+        assertThat(writer).isNotNull();
+        assertThat(writer.getContext()).isEqualTo(new Tuple2<>(List.of("Just halved '8'", "Just halved '4'"), 2));
+    }
+
+    @Test
+    public void testGetValue() {
+        Writer<Integer> writer = Writer.of("Two", 2);
+        Integer value = writer.getValue();
+
+        assertThat(value).isEqualTo(2);
+    }
+
+    @Test
+    public void testGetLogs() {
+        Writer<Integer> writer = Writer.of("Two", 2);
+        List<String> logs = writer.getLogs();
+
+        assertThat(logs).isEqualTo(List.of("Two"));
+    }
+
+    @Test
+    public void testGetContext() {
+        Writer<Integer> writer = Writer.of("Two", 2);
+        Tuple2<List<String>, Integer> context = writer.getContext();
+
+        assertThat(context).isNotNull();
+        assertThat(context._1).isEqualTo(List.of("Two"));
+        assertThat(context._2).isEqualTo(2);
+    }
+
+    @Test
+    public void testToString() {
+        Writer<Integer> writer = Writer.of("2!", 2);
+
+        assertThat(writer).isNotNull();
+        assertThat(writer.toString()).isEqualTo("Writer(2!, 2)");
+    }
+
+    private Writer<Integer> half(Integer i) {
+        return Writer.of("Just halved '"+i+"'", i / 2);
+    }
+
 }
